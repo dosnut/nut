a83e5f328ab44fb5e0666c234fc1dcbc62d43304
Daniel Bahrdt <d.bahrdt@gmx.de>
2007-07-30 16:46:43 +0200

    libnut_type kompiliert jetzt ohne Probleme

--- libnut/libnut_types.cpp
+++ libnut/libnut_types.cpp
@@ -10,8 +10,11 @@ QDBusArgument &operator<< (QDBusArgument &argument, const libnut_SelectConfig & 
 }
 const QDBusArgument &operator>> (const QDBusArgument &argument, libnut_SelectConfig &selconf) {
     quint32 hostaddress;
+    uchar selconf_type;
     argument.beginStructure();
-    argument >> selconf.type >> selconf.useMAC >> selconf.essid >> hostaddress;
+    argument >> selconf_type;
+    selconf.type = (libnut_SelectType) selconf_type;
+    argument >> selconf.useMAC >> selconf.essid >> hostaddress;
     selconf.arpIP = QHostAddress::QHostAddress(hostaddress);
     argument.endStructure();
     return argument;
@@ -34,15 +37,21 @@ QDBusArgument &operator<< (QDBusArgument &argument, const libnut_EnvironmentProp
     argument.beginStructure();
     argument << envprop.active << envprop.name;
     argument << envprop.currentSelection.type << envprop.currentSelection.useMAC;
-    argument << envprop.currentSelection.arpIP << envprop.currentSelection.essid;
+    argument << envprop.currentSelection.arpIP.toIPv4Address() << envprop.currentSelection.essid;
     argument.endStructure();
     return argument;
 };
 const QDBusArgument &operator>> (const QDBusArgument &argument, libnut_EnvironmentProperties &envprop) {
+    uchar selconf_type;
+    quint32 hostaddress;
     argument.beginStructure();
     argument >> envprop.active >> envprop.name;
-    argument >> envprop.currentSelection.type >> envprop.currentSelection.useMAC;
-    argument >> envprop.currentSelection.arpIP >> envprop.currentSelection.essid;
+    argument >> selconf_type;
+    envprop.currentSelection.type = (libnut_SelectType) selconf_type;
+    argument >> envprop.currentSelection.useMAC;
+    argument >> hostaddress;
+    envprop.currentSelection.arpIP = QHostAddress::QHostAddress(hostaddress);
+    argument >> envprop.currentSelection.essid;
     argument.endStructure();
     return argument;
 }
