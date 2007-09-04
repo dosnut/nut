d7d5ad9a409fce04b2097382fbb78a67d605fb6e
Daniel Bahrdt <d.bahrdt@gmx.de>
2007-09-04 19:13:30 +0200

    [libnut] scheiss git

--- libnut/libnut_cli.cpp
+++ libnut/libnut_cli.cpp
@@ -617,7 +617,6 @@ CInterface::CInterface(CEnvironment * parent, QDBusObjectPath dbusPath) : CLibNu
 	dbusConnection = parent->dbusConnection;
 	dbusConnectionInterface = parent->dbusConnectionInterface;
 	dbusInterface = new DBusInterfaceInterface_IPv4(NUT_DBUS_URL, dbusPath.path(), *dbusConnection, this);
-	//dbusInterface = new DBusInterfaceInterface(NUT_DBUS_URL, dbusPath.path(), *dbusConnection, this);
 	serviceCheck(dbusConnectionInterface);
 	//Get properties:
 	QDBusReply<InterfaceProperties> replyprops = dbusInterface->getProperties();
