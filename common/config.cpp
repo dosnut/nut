f3f107d182f00ccc50aae90ed10d119e4235e5a1
Stefan Bühler <stbuehler@web.de>
2007-09-11 12:14:20 +0200

    [COMMON] Fix QHostAddress/DBus operator <</>>

--- common/config.cpp
+++ common/config.cpp
@@ -13,13 +13,17 @@
 #include "config.h"
 
 QDBusArgument &operator<< (QDBusArgument &argument, const QHostAddress &data) {
+	argument.beginStructure();
 	argument << data.toString();
+	argument.endStructure();
 	return argument;
 }
 const QDBusArgument &operator>> (const QDBusArgument &argument, QHostAddress &data) {
+	argument.beginStructure();
 	QString addr;
 	argument >> addr;
 	data = QHostAddress(addr);
+	argument.endStructure();
 	return argument;
 }
 
