8647ed554d147122e46fc6580a031a6c44b49c9d
Stefan Bühler <stbuehler@web.de>
2007-10-22 11:36:53 +0200

    [NUTS] Added support for device adding/removing.

--- nuts/hardware_ext.c
+++ nuts/hardware_ext.c
@@ -25,19 +25,19 @@
 #include <arpa/inet.h>
 
 struct nla_policy ifla_policy[IFLA_MAX+1] = {
-//  [IFLA_IFNAME]           = { .type = NLA_STRING, .maxlen = IFNAMSIZ-1 },
-    [IFLA_MAP]              = { .minlen = sizeof(struct rtnl_link_ifmap) },
-    [IFLA_MTU]              = { .type = NLA_U32 },
-    [IFLA_TXQLEN]           = { .type = NLA_U32 },
-    [IFLA_WEIGHT]           = { .type = NLA_U32 },
+	[IFLA_IFNAME]           = { .type = NLA_STRING, .maxlen = IFNAMSIZ-1 },
+	[IFLA_MAP]              = { .minlen = sizeof(struct rtnl_link_ifmap) },
+	[IFLA_MTU]              = { .type = NLA_U32 },
+	[IFLA_TXQLEN]           = { .type = NLA_U32 },
+	[IFLA_WEIGHT]           = { .type = NLA_U32 },
 //    [IFLA_OPERSTATE]        = { .type = NLA_U8 },
 //    [IFLA_LINKMODE]         = { .type = NLA_U8 },
 };
 
 struct nla_policy ifa_ipv4_policy[IFA_MAX+1] = {
-    [IFA_LOCAL]             = { .type = NLA_U32 },
-    [IFA_ADDRESS]           = { .type = NLA_U32 },
-    [IFA_BROADCAST]         = { .type = NLA_U32 },
-    [IFA_ANYCAST]           = { .type = NLA_U32 },
-    [IFA_LABEL]             = { .type = NLA_STRING, .maxlen = IFNAMSIZ - 1 },
+	[IFA_LOCAL]             = { .type = NLA_U32 },
+	[IFA_ADDRESS]           = { .type = NLA_U32 },
+	[IFA_BROADCAST]         = { .type = NLA_U32 },
+	[IFA_ANYCAST]           = { .type = NLA_U32 },
+	[IFA_LABEL]             = { .type = NLA_STRING, .maxlen = IFNAMSIZ - 1 },
 };
