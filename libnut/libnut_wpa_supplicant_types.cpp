d760498a0baa9bc0b7298a1aac212b6a6685e19a
Daniel Bahrdt <d.bahrdt@gmx.de>
2007-10-27 12:52:52 +0200

    [libnut/wpa] types/parsers for config, many name changes. qnut will not compile

--- libnut/libnut_wpa_supplicant_types.cpp
+++ libnut/libnut_wpa_supplicant_types.cpp
@@ -2,33 +2,165 @@
 
 namespace libnut {
 
-QString toString(wps_protocols proto) {
-//{WKI_UNDEFINED=0, WKI_WPA=1, WKI_RSN=2} wps_protocols;
-	return QString("%1 %2").arg(((WKI_WPA == proto) ? "WPA" : ""),((WKI_RSN == proto) ? "RSN" : ""));
+
+wps_group_ciphers toGroupCiphers(wps_ciphers cip) {
+	//{GCI_CCMP=2, GCI_TKIP=4, GCI_WEP104=8, GCI_WEP40=16, GCI_ALL=31} wps_group_ciphers;
+	//{CI_UNDEFINED=0, CI_NONE=1, CI_CCMP=2, CI_TKIP=4, CI_WEP104=8, CI_WEP40=16, CI_WEP=24} wps_ciphers;
+	if ((int) cip >=2) {
+		return ((wps_group_ciphers) cip);
+	}
+	else {
+		return WGC_DEF;
+	}
 }
-QString toString(CIPHERS cip) {
+wps_pairwise_ciphers toPairwiseCiphers(wps_ciphers cip) {
+	//{PCI_NONE=1, PCI_CCMP=2, PCI_TKIP=4, PCI_DEF=6} wps_pairwise_ciphers;
+	if ((int) cip >= 1 && (int) cip <= 7) {
+		return ((wps_pairwise_ciphers) cip);
+	}
+	else {
+		return WPC_DEF;
+	}
+}
+wps_key_managment toKeyManagment(wps_authentication auth) {
+	//{WA_PLAIN=1,WA_WPA_PSK=2,WA_WPA2_PSK=4, WA_WPA_EAP=8, WA_WPA2_EAP=16, WA_IEEE8021X=32}
+	//{WKM_NONE=1, WKM_WPA_PSK=2, WKM_WPA_EAP=4, WKM_IEEE8021X=8} wps_key_managment;
+	int key = 0;
+	if (WA_PLAIN & auth) {
+		key = (key  | WKM_NONE) ;
+	}
+	if (WA_WPA_PSK & auth) {
+		key = (key | WKM_WPA_PSK);
+	}
+	if (WA_WPA2_PSK & auth) {
+		key = (key | WKM_WPA_PSK);
+	}
+	if (WA_WPA_EAP & auth) {
+		key = (key | WKM_WPA_EAP);
+	}
+	if (WA_WPA2_EAP & auth) {
+		key = (key | WKM_WPA_EAP);
+	}
+	if (WA_IEEE8021X & auth) {
+		key = (key | WKM_IEEE8021X);
+	}
+	return ((wps_key_managment) key);
+}
+wps_auth_algs toAuthAlgs(wps_authentication auth) {
+	//{WAA_UNDEFINED=0, WAA_OPEN=1, WAA_SHARED=2, WAA_LEAP=4} wps_auth_algs;
+	int algs = 0;
+	if (WA_PLAIN & auth) {
+		algs = (algs  | WAA_SHARED) ;
+	}
+	if (WA_WPA_PSK & auth) {
+		algs = (algs | WAA_OPEN);
+	}
+	if (WA_WPA2_PSK & auth) {
+		algs = (algs | WAA_OPEN);
+	}
+	if (WA_WPA_EAP & auth) {
+		algs = (algs | (WAA_OPEN | WAA_LEAP) );
+	}
+	if (WA_WPA2_EAP & auth) {
+		algs = (algs | (WAA_OPEN | WAA_LEAP) );
+	}
+	if (WA_IEEE8021X & auth) {
+		algs = (algs | (WAA_OPEN | WAA_LEAP) );
+	}
+	return ((wps_auth_algs) algs);
+	
+}
+wps_protocols toProtocols(wps_authentication auth) {
+	//{WKI_UNDEFINED=-1, WKI_WPA=1, WKI_RSN=2,WKI_DEF=3} wps_protocols;
+	int proto = 0;
+	if (WA_PLAIN & auth) {
+		proto = (proto  | WP_DEF) ;
+	}
+	if (WA_WPA_PSK & auth) {
+		proto = (proto | WP_WPA);
+	}
+	if (WA_WPA2_PSK & auth) {
+		proto = (proto | WP_RSN);
+	}
+	if (WA_WPA_EAP & auth) {
+		proto = (proto | WP_WPA);
+	}
+	if (WA_WPA2_EAP & auth) {
+		proto = (proto | WP_RSN);
+	}
+	if (WA_IEEE8021X & auth) {
+		proto = (proto | WP_DEF);
+	}
+	return ((wps_protocols) proto);
+}
+
+QString toString(wps_ciphers cip) {
 //{CI_UNDEFINED=0, CI_NONE=1, CI_CCMP=2, CI_TKIP=4, CI_WEP104=8, CI_WEP40=16, CI_WEP=32} CIPHERS;
 	QString ret = "";
-	if (CI_NONE & cip) {
+	if (WC_NONE & cip) {
 		ret.append("NONE ");
 	}
-	if (CI_CCMP & cip) {
+	if (WC_CCMP & cip) {
 		ret.append("CCMP ");
 	}
-	if (CI_TKIP & cip) {
+	if (WC_TKIP & cip) {
 		ret.append("TKIP ");
 	}
-	if (CI_WEP104 & cip) {
+	if (WC_WEP104 & cip) {
 		ret.append("WEP104 ");
 	}
-	if (CI_WEP40 & cip) {
+	if (WC_WEP40 & cip) {
 		ret.append("WEP40 ");
 	}
 	return ret;
 }
-QString toString(KEYMGMT keym) {
-	//KEYMGMT_PLAIN=0, KEYMGMT_WPA_PSK=2, KEYMGMT_WPA2_PSK=4, KEYMGMT_WPA_EAP=8, KEYMGMT_WPA2_EAP=16, KEYMGMT_IEEE8021X=32, KEYMGMT_UNDEFINED=64} KEYMGMT;
-	return QString();
+QString toString(wps_group_ciphers cip) {
+	//{GCI_CCMP=2, GCI_TKIP=4, GCI_WEP104=8, GCI_WEP40=16, GCI_ALL=31} wps_group_ciphers;
+	QString ret;
+	if (cip & WGC_CCMP) {
+		ret.append("CCMP ");
+	}
+	if (cip & WGC_TKIP) {
+		ret.append("TKIP ");
+	}
+	if (cip & WGC_WEP104) {
+		ret.append("WEP104 ");
+	}
+	if (cip & WGC_WEP40) {
+		ret.append("WEP40");
+	}
+	return ret;
+}
+QString toString(wps_pairwise_ciphers cip) {
+	//{PCI_NONE=1, PCI_CCMP=2, PCI_TKIP=4} wps_pairwise_ciphers;
+	QString ret;
+	if (cip & WPC_NONE) {
+		ret.append("NONE ");
+	}
+	if (cip & WPC_CCMP) {
+		ret.append("CCMP");
+	}
+	if(cip & WPC_TKIP) {
+		ret.append("TKIP");
+	}
+	return ret;
+}
+QString toString(wps_key_managment keym) {
+	//{WKM_NONE=1, WKM_WPA_PSK=2, WKM_WPA_EAP=4, WKM_IEEE8021X=8} wps_key_managment;
+	QString ret;
+	if (keym & WKM_NONE) {
+		ret.append("NONE ");
+	}
+	if (keym & WKM_WPA_PSK) {
+		ret.append("WPA-PSK ");
+	}
+	if (keym & WKM_WPA_EAP) {
+		ret.append("WPA-EAP");
+	}
+	if (keym & WKM_IEEE8021X) {
+		ret.append("IEEE8021X");
+	}
+	return ret;
 }
 
 QString toString(wps_auth_algs algs) {
@@ -45,9 +177,16 @@ QString toString(wps_auth_algs algs) {
 	}
 	return ret;
 }
-QString toString(EAPOL_FLAGS flags) {
+
+QString toString(wps_protocols proto) {
+//{WKI_UNDEFINED=0, WKI_WPA=1, WKI_RSN=2} wps_protocols;
+	return QString("%1 %2").arg(((WP_WPA == proto) ? "WPA" : ""),((WP_RSN == proto) ? "RSN" : ""));
+}
+
+QString toString(wps_eapol_flags flags) {
 	return QString::number((int) flags);
 }
+
 QString toString(EAP_METHOD method) {
 	//{EAP_ALL=127, EAPM_MD5=1,EAPM_MSCHAPV2=2,EAPM_OTP=4,EAPM_GTC=8,EAPM_TLS=16,EAPM_PEAP=32,EAPM_TTLS=64} EAP_METHOD;
 	QString ret;
@@ -109,20 +248,20 @@ wps_network_config::wps_network_config() {
 			priority = 0;
 			mode = 0; //0 = infrastructure (Managed) mode, i.e., associate with an AP (default) 1 = IBSS (ad-hoc, peer-to-peer)
 			frequency = 0; //no default, but 0 is not a working value
-			proto = WKI_UNDEFINED; //list of accepted protocols TODO: implement
-			key_mgmt = KEYMGMT_UNDEFINED; // list of accepted authenticated key management protocols
+			proto = WP_UNDEFINED; //list of accepted protocols TODO: implement
+			key_mgmt = WKM_UNDEFINED; // list of accepted authenticated key management protocols
 			auth_alg = WAA_UNDEFINED; //list of allowed IEEE 802.11 authentication algorithms TODO:implement
-			pairwise = CI_UNDEFINED; //list of accepted pairwise (unicast) ciphers for WPA (CCMP,TKIP,NONE)
-			group = CI_UNDEFINED; //list of accepted group (broadcast/multicast) ciphers for WPA (CCMP;TKIP;WEP104/40)
+			pairwise = WPC_DEF; //list of accepted pairwise (unicast) ciphers for WPA (CCMP,TKIP,NONE)
+			group = WGC_DEF; //list of accepted group (broadcast/multicast) ciphers for WPA (CCMP;TKIP;WEP104/40)
 			QString psk = QString(); //WPA preshared key; 256-bit pre-shared key
-			eapol_flags = (EAPOL_FLAGS) (EAP_DYN_UNICAST_WEP | EAP_BCAST_WEP);
+			eapol_flags = (wps_eapol_flags) (EAPF_DYN_UNICAST_WEP | EAPF_BCAST_WEP);
 			mixed_cell = false; //This option can be used to configure whether so called mixed
 			proactive_key_caching = false; //Enable/disable opportunistic PMKSA caching for WPA2.
 			wep_key0 = QString(); //Static WEP key (ASCII in double quotation, hex without)
 			wep_key1 = QString();
 			wep_key2 = QString();
 			wep_key3 = QString();
-			wep_tx_keyidx = WKI_UNDEFINED; //Default WEP key index (TX) (0..3) TODO: implement
+			wep_tx_keyidx = -1; //Default WEP key index (TX) (0..3) TODO: implement
 			peerkey = false; //Whether PeerKey negotiation for direct links (IEEE 802.11e DLS) is allowed.
 }
 wps_network_config::~wps_network_config() {
--- libnut/libnut_wpa_supplicant_types.h
+++ libnut/libnut_wpa_supplicant_types.h
@@ -10,23 +10,55 @@
 namespace libnut {
 
 	typedef enum {WNF_NONE=0, WNF_CURRENT=1} wps_network_flags;
-	typedef enum {CI_UNDEFINED=0, CI_NONE=1, CI_CCMP=2, CI_TKIP=4, CI_WEP104=8, CI_WEP40=16, CI_WEP=32} CIPHERS;
-	typedef enum {KEYMGMT_PLAIN=0, KEYMGMT_WPA_PSK=2, KEYMGMT_WPA2_PSK=4, KEYMGMT_WPA_EAP=8, KEYMGMT_WPA2_EAP=16, KEYMGMT_IEEE8021X=32, KEYMGMT_UNDEFINED=64} KEYMGMT;
-	typedef enum {WKI_UNDEFINED=0, WKI_WPA=1, WKI_RSN=2} wps_protocols;
+
+	typedef enum {WC_UNDEFINED=0, WC_NONE=1, WC_CCMP=2, WC_TKIP=4, WC_WEP104=8, WC_WEP40=16, WC_WEP=24} wps_ciphers;
+	typedef enum {WA_PLAIN=1,WA_WPA_PSK=2,WA_WPA2_PSK=4, WA_WPA_EAP=8, WA_WPA2_EAP=16, WA_IEEE8021X=32, WA_DEF=62} wps_authentication;
+	//WA_WPA_PSK = WAA_OPEN && WKI_WPA && KEYMGMT_WPA_PSK && (GCI_CCMP || GCI_TKIP) && (PCI_CCMP || PCI_TKIP)
+	//WA_WPA2_PSK = WAA_OPEN && WKI_RSN && KEYMGMT_WPA_PSK && (GCI_CCMP || GCI_TKIP) && (PCI_CCMP || PCI_TKIP)
+	//WA_WPA_EAP = (WAA_OPEN || WAA_LEAP) && KEYMGMT_WPA_EAP && WKI_WPA && (GCI_CCMP || GCI_TKIP) && (PCI_CCMP || PCI_TKIP)
+	//WA_WPA2_EAP = (WAA_OPEN || WAA_LEAP) && KEYMGMT_WPA_EAP && WKI_RSN && (GCI_CCMP || GCI_TKIP) && (PCI_CCMP || PCI_TKIP)
+	//WA_IEEE8021X = (WAA_OPEN || LEAP) && KEYMGMT_IEEE8021X && (WKI_RSN || WKI_WPA) // This is not very clear yet
+
+
+	typedef enum {WP_UNDEFINED=-1, WP_WPA=1, WP_RSN=2, WP_DEF=3} wps_protocols; //RSN=WPA2
+	typedef enum {WGC_CCMP=2, WGC_TKIP=4, WGC_WEP104=8, WGC_WEP40=16, WGC_DEF=31} wps_group_ciphers;
+	typedef enum {WPC_NONE=1, WPC_CCMP=2, WPC_TKIP=4, WPC_DEF=6} wps_pairwise_ciphers;
+	typedef enum {WKM_UNDEFINED=0, WKM_NONE=1, WKM_WPA_PSK=2, WKM_WPA_EAP=4, WKM_IEEE8021X=8, WKM_DEF=6} wps_key_managment;
+	typedef enum {WAA_UNDEFINED=0, WAA_OPEN=1, WAA_SHARED=2, WAA_LEAP=4} wps_auth_algs;
+
 	typedef enum {WI_MSG, WI_REQ,WI_EVENT} wps_interact_type;
 	typedef enum {WR_FAIL, WR_PASSWORD, WR_IDENTITY, WR_NEW_PASSWORD, WR_PIN, WR_OTP, WR_PASSPHRASE} wps_req_type;
 	typedef enum {WE_OTHER, WE_DISCONNECTED, WE_CONNECTED, WE_TERMINATING, WE_PASSWORD_CHANGED, WE_EAP_NOTIFICATION, WE_EAP_STARTED, WE_EAP_METHOD, WE_EAP_SUCCESS, WE_EAP_FAILURE } wps_event_type;
-	typedef enum {WAA_UNDEFINED=0, WAA_OPEN=1, WAA_SHARED=2, WAA_LEAP=4} wps_auth_algs;
-	typedef enum {EAP_NONE=0,EAP_DYN_UNICAST_WEP=1, EAP_BCAST_WEP=2} EAPOL_FLAGS;
+
+	typedef enum {EAPF_NONE=0,EAPF_DYN_UNICAST_WEP=1, EAPF_BCAST_WEP=2,EAPF_DEF=3} wps_eapol_flags;
 	typedef enum {EAP_ALL=127, EAPM_MD5=1,EAPM_MSCHAPV2=2,EAPM_OTP=4,EAPM_GTC=8,EAPM_TLS=16,EAPM_PEAP=32,EAPM_TTLS=64} EAP_METHOD;
+
+	//0001 = 1
+	//0010 = 2
+	//0100 = 4
+	//0100 = 8
+
+	typedef enum {
+	WCF_NONE=0, WCF_SSID=1,WCF_BSSID=2,WCF_DISABLED=4,WCF_ID_STR=8,
+	WCF_SCAN_SSID=16, WCF_PRIORITY=32, WCF_MODE=64, WCF_FREQ=128,
+	WCF_PROTO=256, WCF_KEYMGMT=512, WCF_AUTH_ALG=1024, WCF_PAIRWISE=2048,
+	WCF_GROUP=0x0000001000, WCF_PSK=0x0000002000, WCF_EAPOL_FLAGS=0x0000004000, WCF_MIXED_CELL=0x0000008000,
+	WCF_PROA_KEY_CACHING=0x00000010000, WCF_WEP_KEY0=0x0000020000, WCF_WEP_KEY1=0x0000040000, WCF_WEP_KEY2=0x0000080000,
+	WCF_WEP_KEY3=0x0000100000, WCF_WEP_KEY_IDX=0x0000200000, WCF_PEERKEY=0x0000400000, WCF_ALL=0x0000FFFFFF
+	}wps_netconfig_failures;
+
+	struct wps_netconfig_status {
+		wps_netconfig_failures failures;
+		int id;
+	};
 
 	struct wps_scan {
 		nut::MacAddress bssid;
 		QString ssid;
 		int freq;
 		int level;
-		CIPHERS ciphers;
-		KEYMGMT key_mgmt;
+		wps_ciphers ciphers;
+		wps_authentication auth;
 	};
 	struct wps_variable;
 	typedef QList<wps_variable> wps_MIB;
@@ -52,9 +84,9 @@ namespace libnut {
 		nut::MacAddress bssid;
 		QString ssid;
 		int id;
-		CIPHERS pairwise_cipher;
-		CIPHERS group_cipher;
-		KEYMGMT key_mgmt;
+		wps_pairwise_ciphers pairwise_cipher;
+		wps_group_ciphers group_cipher;
+		wps_key_managment key_mgmt;
 		WPA_STATE wpa_state;
 		QHostAddress ip_address;
 		PAE_STATE pae_state;
@@ -96,13 +128,30 @@ namespace libnut {
 		int id;
 	};
 
-	QString toString(wps_protocols proto);
-	QString toString(CIPHERS cip);
-	QString toString(KEYMGMT keym);
-	QString toString(wps_req_type reqt);
+
+	//Conversion functions
+	wps_group_ciphers toGroupCiphers(wps_ciphers cip);
+	wps_pairwise_ciphers toPairwiseCiphers(wps_ciphers cip);
+	wps_key_managment toKeyManagment(wps_authentication auth);
+	wps_auth_algs toAuthAlgs(wps_authentication auth);
+	wps_protocols toProtocols(wps_authentication auth);
+
+
+	QString toString(wps_group_ciphers cip);
+	QString toString(wps_pairwise_ciphers cip);
+	QString toString(wps_key_managment keym);
 	QString toString(wps_auth_algs algs);
-	QString toString(EAPOL_FLAGS flags);
+	QString toString(wps_protocols proto);
+
+
+	QString toString(wps_ciphers cip);
+	QString toString(wps_authentication auth);
+	QString toString(wps_req_type reqt);
+	
+	QString toString(wps_eapol_flags flags);
 	QString toString(EAP_METHOD method);
+	
+
 	
 	struct wps_network {
 		int id;
@@ -123,19 +172,19 @@ namespace libnut {
 			bool mode; //0 = infrastructure (Managed) mode, i.e., associate with an AP (default) 1 = IBSS (ad-hoc, peer-to-peer)
 			int frequency;
 			wps_protocols proto; //list of accepted protocols TODO: implement
-			KEYMGMT key_mgmt; // list of accepted authenticated key management protocols
+			wps_key_managment key_mgmt; // list of accepted authenticated key management protocols
 			wps_auth_algs auth_alg; //list of allowed IEEE 802.11 authentication algorithms TODO:implement
-			CIPHERS pairwise; //list of accepted pairwise (unicast) ciphers for WPA (CCMP,TKIP,NONE)
-			CIPHERS group; //list of accepted group (broadcast/multicast) ciphers for WPA (CCMP;TKIP;WEP104/40)
+			wps_pairwise_ciphers pairwise; //list of accepted pairwise (unicast) ciphers for WPA (CCMP,TKIP,NONE)
+			wps_group_ciphers group; //list of accepted group (broadcast/multicast) ciphers for WPA (CCMP;TKIP;WEP104/40)
 			QString psk; //WPA preshared key; 256-bit pre-shared key
-			EAPOL_FLAGS eapol_flags; // IEEE 802.1X/EAPOL options (bit field) TODO:implement
+			wps_eapol_flags eapol_flags; // IEEE 802.1X/EAPOL options (bit field) TODO:implement
 			bool mixed_cell; //This option can be used to configure whether so called mixed
 			bool proactive_key_caching; //Enable/disable opportunistic PMKSA caching for WPA2.
 			QString wep_key0; //Static WEP key (ASCII in double quotation, hex without)
 			QString wep_key1;
 			QString wep_key2;
 			QString wep_key3;
-			uchar wep_tx_keyidx; //Default WEP key index (TX) (0..3) TODO: implement
+			char wep_tx_keyidx; //Default WEP key index (TX) (0..3) TODO: implement
 			bool peerkey; //Whether PeerKey negotiation for direct links (IEEE 802.11e DLS) is allowed.
 	};
 	class wps_eap_network_config: wps_network_config {
