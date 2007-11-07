f339f44e3a3a373538a587a80b6ebafa94487c50
Oliver Groß <neokensuke@freenet.de>
2007-11-07 12:09:29 +0100

    [libnut] using now parent of QObject

--- libnut/libnut_cli.h
+++ libnut/libnut_cli.h
@@ -111,7 +111,7 @@ namespace libnut {
 		friend class CInterface;
 		friend class DBusDeviceInterface;
 	private:
-		CDeviceManager * parent;
+		//CDeviceManager * parent;
 		QDBusObjectPath dbusPath;
 		QDBusObjectPath dbusActiveEnvironment;
 		QHash<QDBusObjectPath, CEnvironment*> dbusEnvironments;
@@ -164,7 +164,7 @@ namespace libnut {
 		friend class CInterface;
 		friend class DBusEnvironmentInterface;
 	private:
-		CDevice * parent;
+		//CDevice * parent;
 		QDBusObjectPath dbusPath;
 		CLog * log;
 		QHash<QDBusObjectPath, CInterface *> dbusInterfaces;
@@ -204,7 +204,7 @@ namespace libnut {
 		friend class CEnvironment;
 		friend class DBusInterfaceInterface_IPv4;
 	private:
-		CEnvironment * parent;
+		//CEnvironment * parent;
 		QDBusObjectPath dbusPath;
 		CLog * log;
 		DBusInterfaceInterface_IPv4 * dbusInterface;
@@ -218,7 +218,8 @@ namespace libnut {
 		QHostAddress netmask;
 		QHostAddress gateway;
 		QList<QHostAddress> dnsserver;
-
+		
+		nut::IPv4UserConfig getUserConfig();
 		nut::IPv4Config getConfig() { return dbusConfig; }
 		nut::IPv4Config config() { return dbusConfig; } //wants qnut wants it like that
 
@@ -232,7 +233,6 @@ namespace libnut {
 		void setGateway(QHostAddress & address);
 		bool needUserSetup();
 		bool setUserConfig(const nut::IPv4UserConfig &userConfig);
-		nut::IPv4UserConfig getUserConfig();
 		
 	signals:
 		void stateChanged(InterfaceState state);
