139c9e625ca297fe65af35a22cae1ae708de6cf6
Oliver Groß <neokensuke@freenet.de>
2007-12-06 12:39:36 +0100

    [QNUT] script settings docs

--- qnut/scriptsettings.h
+++ qnut/scriptsettings.h
@@ -1,9 +1,6 @@
 //
 // C++ Interface: deviceconfiguration
 //
-// Description: 
-//
-//
 // Author: Oliver Groß <z.o.gross@gmx.de>, (C) 2007
 //
 // Copyright: See COPYING file that comes with this distribution
@@ -18,15 +15,28 @@
 namespace qnut {
 	class CDeviceDetails;
 	
+	/**
+	 * @brief CScriptSettings provides a dialog to configure scripting flags.
+	 * @author Oliver Groß <z.o.gross@gmx.de>
+	 * 
+	 * On creation, the CScriptSettings sets up the user interface to configure scripting flags.
+	 * It provides a public function to open the dialog for an existing instance of CDeviceDetails.
+	 */
 	class CScriptSettings : public QDialog {
 		Q_OBJECT
-	protected:
+	private:
 		Ui::scrset ui;
 	public:
+		/**
+		 * @brief Opens the dialog and returns true if changes are made.
+		 * @param deviceoptions pointer to an existing instance of CDeviceDetails
+		 */
 		bool execute(CDeviceDetails * deviceoptions);
-		
-		CScriptSettings(QWidget *parent = 0);
-		~CScriptSettings();
+		/**
+		 * @brief Creates the object and initializes its user interface.
+		 * @param parent parent widget
+		 */
+		CScriptSettings(QWidget * parent = 0);
 	};
 };
 
