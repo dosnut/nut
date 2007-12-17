a42bd839c21b5a4f4f3c496932088b6ec86135a4
Daniel Bahrdt <d.bahrdt@gmx.de>
2007-12-17 13:37:03 +0100

    [nut_cmd] added missing files

---
+++ nut_cmd/main.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+using namespace nut_cmd;
+int main(int argc, char * argv[]) {
+
+	libnutcommon::init();
+	QCoreApplication app(argc, argv); //just for parsing, maybe this should be changed (overhead?)
+
+
+	//Open connection to DBus:
+	QDBusConnection connection(QDBusConnection::systemBus());
+	//Check if nuts is running:
+	QDBusReply<bool> reply = connection.interface()->isServiceRegistered(NUT_DBUS_URL);
+	if (reply.isValid()) {
+		if (!reply.value()) {
+			return RETVAL_NUTS_OFF;
+		}
+	}
+	else {
+		return RETVAL_DBUS_ERROR;
+	}
+
+	//Nuts is running, now let's parse our commands
+
+	QStringList rawCmdList = app.arguments();
+	
+	//Transform commands into command list:
+	CommandList cmdList = toCommandList(rawCmdList);
+	return dispatchCommands(cmdList,&connection);
+}
---
+++ nut_cmd/main.h
@@ -0,0 +1,6 @@
+#ifndef NUT_CMD_H
+#define NUT_CMD_H
+#include "nut_cmd_parsers.h"
+#include "nut_cmd_commands.h"
+#include <QCoreApplication>
+#endif
