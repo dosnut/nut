e0828b0a76030e8a7292462604cde4cfd234d69b
Stefan Bühler <stbuehler@web.de>
2008-01-22 12:08:22 +0100

    Release 0.4.6

--- qnut/constants.h
+++ qnut/constants.h
@@ -1,7 +1,7 @@
 #ifndef QNUT_CONSTANTS_H
 #define QNUT_CONSTANTS_H
 
-#define UI_VERSION                      "0.4.5"
+#define UI_VERSION                      "0.4.6"
 #define UI_NAME                         tr("QNUT - Qt client for Network UTility Server (NUTS)")
 
 #define UI_DIR_WORK                     ".qnut/"
