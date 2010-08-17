31b0fb61afcbd8a28db922107a1a3d87538352d6
Oliver Groß <neokensuke@freenet.de>
2010-08-17 14:06:54 +0200

    [libnutwireless] small header cleanup

--- libnutwireless/cwireless.h
+++ libnutwireless/cwireless.h
@@ -5,32 +5,27 @@
 #include "wpa_supplicant.h"
 #include "cwexthw.h"
 #include "cnl80211.h"
-#include <QDebug>
-
-
 
 namespace libnutwireless {
+	class CWireless: public QObject {
+		Q_OBJECT
+		private:
+			QString m_ifname;
+			CWpaSupplicant * m_wpa_supplicant;
+			CWirelessHW * m_wireless_hw;
 
-class CWireless: public QObject {
-	Q_OBJECT
-	private:
-		QString m_ifname;
-		CWpaSupplicant * m_wpa_supplicant;
-		CWirelessHW * m_wireless_hw;
-
-	public:
-		CWireless(QObject * parent, QString ifname);
-		~CWireless();
-		void open();
-		void close();
-		inline CWpaSupplicant * getWpaSupplicant() { return m_wpa_supplicant; }
-		inline CWirelessHW * getHardware() { return m_wireless_hw; }
-	public slots:
-		void scan();
-	signals:
-		void message(QString);
-};
-
+		public:
+			CWireless(QObject * parent, QString ifname);
+			~CWireless();
+			void open();
+			void close();
+			inline CWpaSupplicant * getWpaSupplicant() { return m_wpa_supplicant; }
+			inline CWirelessHW * getHardware() { return m_wireless_hw; }
+		public slots:
+			void scan();
+		signals:
+			void message(QString);
+	};
 }
 #endif
 #endif
--- libnutwireless/cwirelesshw.h
+++ libnutwireless/cwirelesshw.h
@@ -4,34 +4,27 @@
 #ifndef LIBNUT_NO_WIRELESS
 #include "cnetworkconfig.h"
 #include "hwtypes.h"
-#include <QDebug>
-
-
 
 namespace libnutwireless {
-	
-
-
-class CWirelessHW: public QObject {
-	Q_OBJECT
-	public:
-		CWirelessHW(QObject* parent) : QObject(parent) {};
-		~CWirelessHW() {};
-		virtual bool open()=0;
-		virtual void close()=0;
-		virtual void scan()=0;
-		virtual QList<ScanResult> getScanResults()=0;
-		virtual SignalQuality getSignalQuality()=0;
-		virtual void setSignalQualityPollRate(int msec)=0;
-		virtual int getSignalQualityPollRate()=0;
-		virtual QList<quint32> getSupportedChannels()=0;
-		virtual QList<quint32> getSupportedFrequencies()=0;
-	signals:
-		void scanCompleted();
-		void signalQualityUpdated(libnutwireless::SignalQuality sq);
-		void message(QString msg);
-};
-
+	class CWirelessHW: public QObject {
+		Q_OBJECT
+		public:
+			CWirelessHW(QObject* parent) : QObject(parent) {};
+			~CWirelessHW() {};
+			virtual bool open()=0;
+			virtual void close()=0;
+			virtual void scan()=0;
+			virtual QList<ScanResult> getScanResults()=0;
+			virtual SignalQuality getSignalQuality()=0;
+			virtual void setSignalQualityPollRate(int msec)=0;
+			virtual int getSignalQualityPollRate()=0;
+			virtual QList<quint32> getSupportedChannels()=0;
+			virtual QList<quint32> getSupportedFrequencies()=0;
+		signals:
+			void scanCompleted();
+			void signalQualityUpdated(libnutwireless::SignalQuality sq);
+			void message(QString msg);
+	};
 }
 #endif
 #endif
