dosnut/nut

commit 3c5238d9fca093a7632ccd3583564a0b12d17544

[nuts] use process manager to keep track of running child processes before terminating

nuts/events.cpp
@@ -11,83 +11,78 @@
 //
 #include "events.h"
 
+#include "processmanager.h"
+
 #include <QList>
 #include <QFile>
 
 namespace nuts {
-	Events::Events(QObject *parent)
-	: QObject(parent) {
+	Events::Events(ProcessManager* processManager)
+	: m_processManager(processManager) {
 	}
 
 	Events::~Events() {
 	}
 
-	void Events::start(QStringList &environment, const QString &event, const QString &device, const QString &env, int iface) {
+	void Events::start(QProcessEnvironment const& environment, QString const& event, QString const& device, QString const& env, int iface) {
 		QStringList args;
 		args << event << device;
 		if (!env.isNull()) args << env;
 		if (iface != -1) args << QString("%1").arg(iface);
-		QProcess *process = new QProcess(this);
-		environment << QString("NUT_EVENT=%1").arg(event);
-		process->setEnvironment(environment);
-		process->start("/etc/nuts/dispatch", args);
-		connect(process, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished), process, &QObject::deleteLater);
-		connect(process, static_cast<void(QProcess::*)(QProcess::ProcessError)>(&QProcess::error), process, &QObject::deleteLater);
+
+		m_processManager->startProgram(environment, "/etc/nuts/dispatch", args);
 	}
 
-	void setupEnvironment(QStringList &environment) {
-		environment = QProcess::systemEnvironment();
-	}
+	namespace {
+		void setupEnvironment(QProcessEnvironment &environment) {
+			environment = QProcessEnvironment::systemEnvironment();
+		}
 
-	void setupEnvironment(QStringList &environment, Device *dev) {
-		environment
-			<< QString("NUT_DEVICE=%1").arg(dev->getName())
-			<< QString("NUT_HAS_WLAN=%1").arg(dev->isAir())
-			<< QString("NUT_ESSID=%1").arg(dev->getEssid())
-			;
-	}
+		void setupEnvironment(QProcessEnvironment &environment, Device *dev) {
+			environment.insert("NUT_DEVICE", dev->getName());
+			environment.insert("NUT_HAS_WLAN", QString::number(dev->isAir()));
+			environment.insert("NUT_ESSID", dev->getEssid());
+		}
 
-	void setupEnvironment(QStringList &environment, Environment *env) {
-		environment
-			<< QString("NUT_ENVIRONMENT=%1").arg(env->getName())
-			<< QString("NUT_ENVIRONMENT_INDEX=%1").arg(env->getID())
-			;
-	}
+		void setupEnvironment(QProcessEnvironment &environment, Environment *env) {
+			environment.insert("NUT_ENVIRONMENT",env->getName());
+			environment.insert("NUT_ENVIRONMENT_INDEX", QString::number(env->getID()));
+		}
 
-	void setupEnvironment(QStringList &environment, Interface_IPv4* iface) {
-		environment
-			<< QString("NUT_IP=%1").arg(iface->getIP().toString())
-			<< QString("NUT_NETMASK=%1").arg(iface->getNetmask().toString())
-			<< QString("NUT_LOCALDOMAIN=%1").arg(iface->getLocalDomain())
-			<< QString("NUT_STATUS=%1").arg(libnutcommon::toString(iface->getState()));
-		if (!iface->getGateway().isNull())
-			environment << QString("NUT_GATEWAY=%1").arg(iface->getGateway().toString());
-		QStringList dnsServers;
-		foreach (QHostAddress dns, iface->getDnsServers())
-			dnsServers << dns.toString();
-		environment << QString("NUT_DNSSERVER=%1").arg(dnsServers.join(","));
+		void setupEnvironment(QProcessEnvironment &environment, Interface_IPv4* iface) {
+			environment.insert("NUT_IP", iface->getIP().toString());
+			environment.insert("NUT_NETMASK", iface->getNetmask().toString());
+			environment.insert("NUT_LOCALDOMAIN", iface->getLocalDomain());
+			environment.insert("NUT_STATUS", libnutcommon::toString(iface->getState()));
+			if (!iface->getGateway().isNull())
+				environment.insert("NUT_GATEWAY", iface->getGateway().toString());
+			QStringList dnsServers;
+			foreach (QHostAddress dns, iface->getDnsServers())
+				dnsServers << dns.toString();
+			environment.insert("NUT_DNSSERVER", dnsServers.join(","));
+		}
 	}
 
 	void Events::deviceAdded(QString devName, Device *dev) {
-		QStringList e;
+		QProcessEnvironment e;
 		setupEnvironment(e);
 		setupEnvironment(e, dev);
 		start(e, "deviceAdd", devName);
 	}
 	void Events::deviceRemoved(QString devName, Device *dev) {
-		QStringList e;
+		QProcessEnvironment e;
 		setupEnvironment(e);
 		setupEnvironment(e, dev);
 		start(e, "deviceRemove", devName);
 	}
 	void Events::stateChanged(libnutcommon::DeviceState newState, libnutcommon::DeviceState oldState, Device* dev) {
-		QStringList e;
+		QProcessEnvironment e;
 		setupEnvironment(e);
 		setupEnvironment(e, dev);
 		QString nState = libnutcommon::toString(newState), oState = libnutcommon::toString(oldState);
-		e << QString("NUT_OLD_STATE=%1").arg(oState)
-		  << QString("NUT_NEW_STATE=%1").arg(nState)
-		  << QString("NUT_STATE=%1").arg(nState);
+		e.insert("NUT_OLD_STATE", oState);
+		e.insert("NUT_NEW_STATE", nState);
+		e.insert("NUT_STATE", nState);
 		QString envName;
 		int e_idx = dev->getEnvironment();
 		if (e_idx >= 0) {
@@ -114,7 +109,7 @@ namespace nuts {
 			default:
 				return;
 		}
-		QStringList e;
+		QProcessEnvironment e;
 		Environment *env = iface->getEnvironment();
 		Device *dev = env->getDevice();
 		setupEnvironment(e);

nuts/events.h
@@ -17,6 +17,8 @@
 #include <libnutcommon/common.h>
 
 namespace nuts {
+	class ProcessManager;
+
 	class Device;
 	class Environment;
 	class Interface_IPv4;
@@ -29,11 +31,11 @@ namespace nuts {
 	class Events : public QObject {
 		Q_OBJECT
 		public:
-			Events(QObject *parent = 0);
+			explicit Events(ProcessManager* processManager);
 			virtual ~Events();
 
 		private:
-			void start(QStringList &environment, const QString &event, const QString &device, const QString &env = QString(), int iface=-1);
+			void start(QProcessEnvironment const& environment, QString const& event, QString const& device, QString const& env = QString(), int iface = -1);
 
 		public:
 			void stateChanged(libnutcommon::DeviceState newState, libnutcommon::DeviceState oldState, Device* device);
@@ -42,6 +44,9 @@ namespace nuts {
 		public slots:
 			void deviceAdded(QString devName, Device *dev);
 			void deviceRemoved(QString devName, Device *dev);
+
+		private:
+			ProcessManager* m_processManager;
 	};
 }
 

nuts/CMakeLists.txt
@@ -26,6 +26,8 @@ add_executable(${PROJECT_NAME}
 	log.cpp
 	log.h
 	main.cpp
+	processmanager.cpp
+	processmanager.h
 	qobject_timer.cpp
 	qobject_timer.h
 	random.cpp

nuts/device.cpp
@@ -45,8 +45,8 @@ static inline void setSockaddrIPv4(struct sockaddr &s, quint32 host = 0, quint16
 namespace nuts {
 	using namespace libnutcommon;
 
-	DeviceManager::DeviceManager(const QString &configFile)
-	: m_config(parseConfig(configFile)) {
+	DeviceManager::DeviceManager(const QString &configFile, ProcessManager* processManager)
+	: m_events(processManager), m_config(parseConfig(configFile)) {
 		connect(&m_hwman, &HardwareManager::gotCarrier, this, &DeviceManager::gotCarrier);
 		connect(&m_hwman, &HardwareManager::lostCarrier, this, &DeviceManager::lostCarrier);
 		connect(&m_hwman, &HardwareManager::newDevice, this, &DeviceManager::newDevice);
@@ -84,11 +84,24 @@ namespace nuts {
 	}
 
 	DeviceManager::~DeviceManager() {
+		// shutdown() should be used to cleanup before
+
 		// manually deleting devices so HardwareManager is available for their destruction;
 		// in ~QObject  (deleteChildren) it is too late for them.
 		foreach (Device* device, m_devices)
 			delete device;
 		m_devices.clear();
+	}
+
+	void DeviceManager::shutdown()
+	{
+		// no new hardware anymore
+		disconnect(&m_hwman, &HardwareManager::newDevice, 0, 0);
+
+		for (QString const& ifName: m_devices.keys()) {
+			Device* device = m_devices.value(ifName, nullptr);
+			if (device) delete device;
+		}
 	}
 
 	void DeviceManager::ca_timer() {

nuts/device.h
@@ -15,6 +15,8 @@
 #include "libnutcommon/common.h"
 
 namespace nuts {
+	class ProcessManager;
+
 	class DBusDeviceManager;
 
 	class DeviceManager;
@@ -43,7 +45,7 @@ namespace nuts {
 		If devices are created later (or are removed) by the kernel, it emits
 		signals if they are managed by the DeviceManager, i.e. are in the config file.
 	 */
-	class DeviceManager : public QObject {
+	class DeviceManager final : public QObject {
 		Q_OBJECT
 	private:
 		Events m_events;
@@ -80,8 +82,10 @@ namespace nuts {
 		 * @brief Constructs the DeviceManager
 		 * @param configFile Path to the configfile.
 		 */
-		DeviceManager(const QString &configFile);
-		virtual ~DeviceManager();
+		explicit DeviceManager(const QString &configFile, ProcessManager* processManager);
+		~DeviceManager();
+
+		void shutdown();
 
 		/**
 		 * @brief Gets all managed devices.

nuts/main.cpp
@@ -5,6 +5,7 @@
 #include "exception.h"
 #include "log.h"
 #include "dbus.h"
+#include "processmanager.h"
 
 #include <QCoreApplication>
 #include <iostream>
@@ -17,6 +18,7 @@ namespace nuts {
 		std::unique_ptr<libnutcommon::DBusManager> dbusManager;
 		std::unique_ptr<SigHandler> sighandler;
 		std::unique_ptr<DeviceManager> devManager;
+		std::unique_ptr<ProcessManager> processManager;
 
 // TODO: log when dbus connection got lost
 //	log << "dbus has been stopped, please restart it";
@@ -24,10 +26,20 @@ namespace nuts {
 
 		try {
 			dbusManager.reset(new libnutcommon::DBusManager(libnutcommon::createDefaultDBusConnection));
-			sighandler.reset(new SigHandler());
-			devManager.reset(new DeviceManager(argc > 1 ? argv[1] : "/etc/nuts/nuts.config"));
+			sighandler.reset(new SigHandler(/* quitOnSignal = */ false));
+			processManager.reset(new ProcessManager());
+			devManager.reset(new DeviceManager(argc > 1 ? argv[1] : "/etc/nuts/nuts.config", processManager.get()));
 			auto dbusDevManager = new DBusDeviceManager(devManager.get());
 			dbusDevManager->connectManager(dbusManager.get());
+
+			QObject::connect(sighandler.get(), &SigHandler::gotQuitSignal, [&processManager, &devManager]() {
+				if (devManager) devManager->shutdown();
+				if (processManager) processManager->shutdown();
+			});
+			QObject::connect(processManager.get(), &ProcessManager::finishedShutdown, [](){
+				QCoreApplication::exit();
+			});
+
 		} catch (Exception &e) {
 			err << "Initialize failed:" << endl
 				<< "    " << e.msg() << endl;
@@ -40,6 +52,7 @@ namespace nuts {
 			return -2;
 		}
 		try {
+			processManager.reset();
 			devManager.reset();
 			sighandler.reset();
 			dbusManager.reset();

nuts/processmanager.cpp
@@ -1,0 +1,146 @@
+#include "processmanager.h"
+
+#include <QChildEvent>
+#include <QDebug>
+
+namespace nuts {
+	Process::Process(ProcessManager* parent, QProcess* process, ShutdownProcessHandling handling, int waitBeforeShutdownMsecs, int killTimeoutMsecs)
+		: QObject(parent)
+		, m_process(process)
+		, m_handling(handling)
+		, m_waitBeforeShutdownMsecs(waitBeforeShutdownMsecs)
+		, m_killTimeoutMsecs(killTimeoutMsecs)
+	{
+		if (m_killTimeoutMsecs > 0) {
+			m_normal_timer.set_timeout(this, m_killTimeoutMsecs);
+		}
+		parent->addProcess(this);
+		connect(m_process, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished), this, &Process::finished);
+		connect(m_process, static_cast<void(QProcess::*)(QProcess::ProcessError)>(&QProcess::error), this, &Process::error);
+	}
+
+	Process::~Process()
+	{
+		static_cast<ProcessManager*>(parent())->removeProcess(this);
+	}
+
+	void Process::timerEvent(QTimerEvent* event) {
+		if (!m_process) return;
+
+		if (m_normal_timer.match(event)) {
+			m_normal_timer.kill(this);
+			m_process->terminate();
+		} else if (m_shutdown_timer.match(event)) {
+			m_shutdown_timer.kill(this);
+			switch (m_handling) {
+			case ShutdownProcessHandling::KillAndDetach:
+				m_process->terminate();
+				detach();
+				break;
+			case ShutdownProcessHandling::Detach:
+				detach();
+				break;
+			case ShutdownProcessHandling::KillAndWait:
+				m_process->terminate();
+				break;
+			}
+		}
+	}
+
+	void Process::detach() {
+		if (m_process) {
+			// leak m_process here - the destructor would block
+			m_process->setParent(nullptr);
+			m_process.clear();
+		}
+		deleteLater();
+	}
+
+	void Process::finished(int exitCode, QProcess::ExitStatus exitStatus)
+	{
+		if (!m_process) return;
+		if (exitStatus == QProcess::CrashExit) {
+			qWarning()
+				<< "Process " << m_process->processId() << " crashed with exit code " << exitCode << "\n"
+				<< "Command: " << m_process->program() << " " << m_process->arguments() << "\n"
+				<< "Environment: " << m_process->environment();
+		} else if (0 != exitCode) {
+			qWarning()
+				<< "Process " << m_process->processId() << " exited with non-zero exit code " << exitCode << "\n"
+				<< "Command: " << m_process->program() << " " << m_process->arguments() << "\n"
+				<< "Environment: " << m_process->environment();
+		}
+		m_process->deleteLater();
+		m_process.clear();
+		deleteLater();
+	}
+
+	void Process::error(QProcess::ProcessError error)
+	{
+		if (!m_process) return;
+		switch (error) {
+		case QProcess::Crashed:
+			// wait for finished signal
+			return;
+		case QProcess::FailedToStart:
+			qWarning()
+				<< "Failed to start command: " << m_process->program() << " " << m_process->arguments() << "\n"
+				<< "Environment: " << m_process->environment();
+			break;
+		default:
+			qWarning()
+				<< "Unknown error with process " << m_process->processId() << "\n"
+				<< "Command: " << m_process->program() << " " << m_process->arguments() << "\n"
+				<< "Environment: " << m_process->environment();
+			break;
+		}
+
+		m_process->kill();
+		m_process->deleteLater();
+		m_process.clear();
+		deleteLater();
+	}
+
+	void Process::shutdown() {
+		m_shutdown_timer.set_timeout(this, m_waitBeforeShutdownMsecs);
+	}
+
+	void ProcessManager::startProgram(const QProcessEnvironment& env, const QString& program, const QStringList& args)
+	{
+		QProcess* process = new QProcess();
+		process->setProcessEnvironment(env);
+		process->start(program, args);
+		new Process(this, process);
+	}
+
+	void ProcessManager::shutdown() {
+		if (m_shutdown) return;
+		m_shutdown = true;
+
+		if (m_processList.empty()) {
+			emit finishedShutdown();
+			return;
+		}
+
+		// copy list
+		auto pl = m_processList;
+		for (auto p: pl) {
+			p->shutdown();
+		}
+	}
+
+	void ProcessManager::addProcess(Process* process)
+	{
+		if (m_shutdown) {
+			qWarning("New process but already shutting down");
+			process->shutdown();
+		} else {
+			m_processList.append(process);
+		}
+	}
+
+	void ProcessManager::removeProcess(Process* process)
+	{
+		if (m_processList.removeOne(process) && m_shutdown && m_processList.empty()) emit finishedShutdown();
+	}
+}

nuts/processmanager.h
@@ -1,0 +1,83 @@
+#ifndef NUTSPROCESSMANAGER_H
+#define NUTSPROCESSMANAGER_H
+
+#pragma once
+
+#include "qobject_timer.h"
+
+#include <memory>
+
+#include <QObject>
+#include <QProcess>
+#include <QList>
+#include <QPointer>
+
+namespace nuts {
+	class ProcessManager;
+	class Process;
+}
+
+namespace nuts {
+	enum class ShutdownProcessHandling {
+		Detach,
+		KillAndWait,
+		KillAndDetach,
+	};
+
+	class Process final : public QObject {
+		Q_OBJECT
+	private:
+		explicit Process(
+			ProcessManager* parent,
+			QProcess *process,
+			ShutdownProcessHandling handling = ShutdownProcessHandling::KillAndDetach,
+			int waitBeforeShutdownMsecs = 3000,
+			int killTimeoutMsecs = -1);
+	public:
+		~Process();
+
+	protected:
+		void timerEvent(QTimerEvent *event) override;
+		void detach();
+
+	private slots:
+		void finished(int exitCode, QProcess::ExitStatus exitStatus);
+		void error(QProcess::ProcessError error);
+
+	private:
+		friend class ProcessManager;
+		void shutdown();
+
+	private:
+		QPointer<QProcess> m_process;
+		ShutdownProcessHandling m_handling;
+		int m_waitBeforeShutdownMsecs;
+		int m_killTimeoutMsecs;
+
+		QObjectTimer m_normal_timer;
+		QObjectTimer m_shutdown_timer;
+	};
+
+	class ProcessManager : public QObject
+	{
+		Q_OBJECT
+	public:
+		void startProgram(const QProcessEnvironment& env, const QString& program, const QStringList& args);
+
+		void shutdown();
+
+	signals:
+		void finishedShutdown();
+
+	private:
+		friend class Process;
+		void addProcess(Process* process);
+		void removeProcess(Process* process);
+
+	private:
+		bool m_shutdown = false;
+		QList<Process*> m_processList;
+	};
+}
+
+#endif // NUTSPROCESSMANAGER_H

commit 57a242a2dd9a79d43a840642747ee68b4d73fa84

[nuts] fix missing NUT_EVENT variable in dispatch script
Regression from: 3c5238d9fca093a7632ccd3583564a0b12d17544

nuts/events.cpp
@@ -10,12 +10,13 @@ namespace nuts {
 	: m_processManager(processManager) {
 	}
 
-	void Events::start(QProcessEnvironment const& environment, QString const& event, QString const& device, QString const& env, int iface) {
+	void Events::start(QProcessEnvironment&& environment, QString const& event, QString const& device, QString const& env, int iface) {
 		QStringList args;
 		args << event << device;
 		if (!env.isNull()) args << env;
 		if (iface != -1) args << QString("%1").arg(iface);
 
+		environment.insert("NUT_EVENT", event);
 		m_processManager->startProgram(environment, "/etc/nuts/dispatch", args);
 	}
 
@@ -53,13 +54,13 @@ namespace nuts {
 		QProcessEnvironment e;
 		setupEnvironment(e);
 		setupEnvironment(e, dev);
-		start(e, "deviceAdd", devName);
+		start(std::move(e), "deviceAdd", devName);
 	}
 	void Events::deviceRemoved(QString devName, Device* dev) {
 		QProcessEnvironment e;
 		setupEnvironment(e);
 		setupEnvironment(e, dev);
-		start(e, "deviceRemove", devName);
+		start(std::move(e), "deviceRemove", devName);
 	}
 	void Events::stateChanged(libnutcommon::DeviceState newState, libnutcommon::DeviceState oldState, Device* dev) {
 		QProcessEnvironment e;
@@ -76,7 +77,7 @@ namespace nuts {
 			setupEnvironment(e, env);
 			envName = env->getName();
 		}
-		start(e, nState, dev->getName(), envName);
+		start(std::move(e), nState, dev->getName(), envName);
 	}
 	void Events::interfaceStatusChanged(libnutcommon::InterfaceState state, Interface_IPv4* iface) {
 		QString event;
@@ -102,6 +103,6 @@ namespace nuts {
 		setupEnvironment(e, dev);
 		setupEnvironment(e, env);
 		setupEnvironment(e, iface);
-		start(e, "ifup", dev->getName(), env->getName(), iface->getIndex());
+		start(std::move(e), "ifup", dev->getName(), env->getName(), iface->getIndex());
 	}
 }

nuts/events.h
@@ -22,7 +22,7 @@ namespace nuts {
 		explicit Events(ProcessManager* processManager);
 
 	private:
-		void start(QProcessEnvironment const& environment, QString const& event, QString const& device, QString const& env = QString(), int iface = -1);
+		void start(QProcessEnvironment&& environment, QString const& event, QString const& device, QString const& env = QString(), int iface = -1);
 
 	public:
 		void stateChanged(libnutcommon::DeviceState newState, libnutcommon::DeviceState oldState, Device* device);